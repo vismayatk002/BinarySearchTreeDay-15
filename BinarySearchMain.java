f8afed831189f632d9f44c1261e7f5590b74199c
vismayatk002 <vismayatk002@gmail.com>
2021-08-13 22:44:37 +0530

    Refactor size method

--- BinarySearchMain.java
+++ BinarySearchMain.java
@@ -2,11 +2,22 @@ public class BinarySearchMain{
 
     public static void main(String[] args){
 
-        MyBinaryNode<Integer> binary = new MyBinaryNode<>();
-        binary.add(10);
-        binary.add(5);
-        binary.add(2);
-        binary.add(1);
-        binary.add(20);
+        MyBinaryNode<Integer> binary = new MyBinaryNode<>(13);
+        binary.add(56);
+        binary.add(30);
+        binary.add(70);
+        binary.add(22);
+        binary.add(40);
+        binary.add(60);
+        binary.add(95);
+        binary.add(11);
+        binary.add(65);
+        binary.add(3);
+        binary.add(16);
+        binary.add(63);
+        binary.add(67);
+        // This node will be excluded since the size exceeded
+        binary.add(98);
+        System.out.println("\n Size : "+binary.getSize());
     }
 }
--- MyBinaryNode.java
+++ MyBinaryNode.java
@@ -1,13 +1,21 @@
 public class MyBinaryNode<T extends Comparable<T>>{
 
     Node<T> root;
+    int size;
+
+    public MyBinaryNode(int size){
+        this.size = size;
+    }
+
     public void add(T key){
 
         this.root = addReccursive(this.root, key);
     }
 
     public Node<T> addReccursive(Node<T> currentRoot, T newKey){
-
+        if(getSize() == this.size){
+            return currentRoot;
+        }
         if(currentRoot == null){
             return new Node<>(newKey);
         }
@@ -21,7 +29,20 @@ public class MyBinaryNode<T extends Comparable<T>>{
         }
         return currentRoot;
     }
-    
+
+    public int getSize(){
+        return getSizeReccursive(this.root);
+    }
+    public int getSizeReccursive(Node<T> currentRoot){
+        int count = 0;
+        if(currentRoot == null){
+            return 0;
+        }
+        else{
+            count = 1 + getSizeReccursive(currentRoot.left) + getSizeReccursive(currentRoot.right);
+        }
+        return count;
+    }
     private class Node<T extends Comparable<T>>{
 
         T key;
